e2b12dc8803ebce21366dd3b0fcdf4730778b9fa
Rafael Ferreira Garcia <rafaelfgx@gmail.com>
2018-10-17 22:35:46 -0300

    .

---
+++ Api.Customers/Startup.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Api.Customers
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder application, IHostingEnvironment environment)
+        {
+            if (environment.IsDevelopment())
+            {
+                application.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                application.UseHsts();
+            }
+
+            application.UseHttpsRedirection();
+            application.UseMvc();
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+    }
+}
